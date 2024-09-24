7504c89c79b92e4a43593ae0bd56926781dfd2fe
MANJIT <manjitchakraborty9@gmail.com>
2024-09-24 22:31:59 +0530

    Add the dynamic page for all

--- app/src/main/java/com/example/geofence/MainActivity.java
+++ app/src/main/java/com/example/geofence/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.geofence;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.EdgeToEdge;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        EdgeToEdge.enable(this);
+
+        Button button1 = findViewById(R.id.button2);
+        Button button2 = findViewById(R.id.button3);
+        Button button3 = findViewById(R.id.button5);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Admin_dashboard.class));
+            }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Employe_dashboard.class));
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Create_organization.class));
+            }
+        });
+
     }
 }
